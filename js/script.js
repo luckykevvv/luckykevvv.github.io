e1a3aa948e3737f26da212d9687254801a4693ba
Jiawen Lu <luckykevvv@gmail.com>
2024-05-20 21:39:56 +1000

    new2

--- js/script.js
+++ js/script.js
@@ -39,4 +39,21 @@ for (const item of dropDowns) {
 	};
 	item.addEventListener('click', onClick);
 }
-                                
+                  
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
+  
+	// If we need pagination
+	pagination: {
+	  el: '.swiper-pagination',
+	},
+  
+	// Navigation arrows
+	navigation: {
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev',
+	},
+  
+  });
